f519b711c815dae04b7baa59907e32d024f5b359
adinaysm <dava0262@gmail.com>
2021-10-01 14:44:23 +0600

    //

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,8 +26,6 @@ public class Main {
         else if (age <=20 && age >=0 && temperature >=0 && temperature <=28) System.out.println("Можно идти гулять");
         else if (age >=45 && temperature >=10 && temperature <25) System.out.println("Можно идти гулять");
         else System.out.println("Оставайтесь дома");
-
-
         System.out.println(age);
         System.out.println();
     }
